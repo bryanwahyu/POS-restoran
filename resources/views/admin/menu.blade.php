33b403ca0ad085fbcd5520791274f68078b793b9
putra <triputra.anu@gmail.com>
2021-01-04 12:44:15 +0700

    add fnumber

--- resources/views/admin/menu.blade.php
+++ resources/views/admin/menu.blade.php
@@ -66,7 +66,7 @@
                 <div class="form-group form-row">
                     <label class="col-3">Harga :</label>
                     <div class="col-8">
-                        <input type="number" class="form-control" id="add-harga">
+                        <input data-type='currency' type="text" class="form-control" id="add-harga">
                     </div>
                 </div>
             </div>
@@ -177,9 +177,10 @@
           $("#tambah-modal").modal('show')
       }
       function tambah_menu(){
+          let gHarga = $('#add-harga').val();
           let data={}
           data.nama=$('#add-nama').val()
-          data.harga=$('#add-harga').val()
+          data.harga= gHarga.replace(",", "");
           data.jenis=$('#add-jenis').val()
           data.foto=addfoto
 
@@ -280,6 +281,50 @@
                editfoto=base64
           })
         });
+
+        function formatNumber(n) {
+          return n.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        }
+        $("input[data-type='currency']").on({
+            keyup: function() {
+                formatCurrency($(this));
+            },
+            blur: function() {
+                formatCurrency($(this), "blur");
+            }
+        });
+        function formatCurrency(input, blur) {
+            var input_val = input.val();
+            if (input_val === "") {
+                return;
+            }
+            var original_len = input_val.length;
+            var caret_pos = input.prop("selectionStart");
+            if (input_val.indexOf(".") >= 0) {
+                var decimal_pos = input_val.indexOf(".");
+                var left_side = input_val.substring(0, decimal_pos);
+                var right_side = input_val.substring((decimal_pos + 1));
+                left_side = formatNumber(left_side);
+                //            right_side = formatNumber(right_side);
+                right_side = formatDesimal(right_side);
+                //            if (blur === "blur") {
+                //                right_side += "00";
+                //            }
+                //            right_side = right_side.substring(0, 2);
+                input_val = left_side + "." + right_side;
+            } else {
+                input_val = formatNumber(input_val);
+                input_val = input_val;
+                // if (blur === "blur") {
+                //     input_val += ".00";
+                // }
+            }
+            input.val(input_val);
+            var updated_len = input_val.length;
+            caret_pos = updated_len - original_len + caret_pos;
+            input[0].setSelectionRange(caret_pos, caret_pos);
+        }
+
     </script>
 
 @endsection
