671f7c864f1f6c1ddb1949a809bc8edc34a1a1c7
Kevin Abrar Khansa <kevariable@gmail.com>
2021-01-31 12:35:37 +0700

    kasir: fix jquery & font awesome move semua ke app.js dan app.scss

--- resources/views/layout/kasir.blade.php
+++ resources/views/layout/kasir.blade.php
@@ -12,13 +12,7 @@
   <title>Kopiin Aja - Dashboard</title>
 
   <!-- Custom fonts for this template-->
-  <link href="{{asset('vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-  <script src="{{asset('vendor/jquery/jquery.min.js')}}"></script>
-  <script src="{{asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-
-  <!-- Core plugin JavaScript-->
-  <script src="{{asset('vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
   <!-- Custom scripts for all pages-->
   <script src="{{asset('js/sb-admin-2.min.js')}}"></script>
@@ -285,6 +279,8 @@
       </div>
     </div>
   </div>
+
+  <script src="{{ asset('js/app.js') }}"></script>
 
   <!-- Bootstrap core JavaScript-->
   <script>
